22660a6a680b324bda4f242dc7d1db26ca38f55c
Mark Rothlisberger <rothlmar@gmail.com>
2023-11-28 12:09:29 -0500

    Update before-disclosure.js with @tracked

--- javascripts/discourse/connectors/before-composer-controls/before-disclosure.js
+++ javascripts/discourse/connectors/before-composer-controls/before-disclosure.js
@@ -1,11 +1,17 @@
 import Component from "@glimmer/component";
+import { tracked } from '@glimmer/tracking'
 
 export default class BeforeDisclosure extends Component {
+  @tracked category = this.args.category;
+  @tracked tags = this.args.tag;
+  
   get processableTags() {
     return "TAGS: " + settings.valid_tags;
   }
 
   static shouldRender(outletArgs, helper) {
+    console.log("Category: ", this.category);
+    console.lgo("Tags: ", this.tags);
     console.log(outletArgs.model.tags);
     return outletArgs.model.tags.length > 0;
   }
