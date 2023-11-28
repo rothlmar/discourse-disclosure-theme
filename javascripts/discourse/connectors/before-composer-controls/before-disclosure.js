89986ec266607c26808bf2bc2f0fc9f1c0905971
Mark Rothlisberger <rothlmar@gmail.com>
2023-11-28 12:52:26 -0500

    Update before-disclosure.js comment out shouldRender

--- javascripts/discourse/connectors/before-composer-controls/before-disclosure.js
+++ javascripts/discourse/connectors/before-composer-controls/before-disclosure.js
@@ -2,10 +2,10 @@ import Component from "@glimmer/component";
 // import { tracked } from '@glimmer/tracking'
 
 export default class BeforeDisclosure extends Component {
-  static shouldRender(outletArgs, helper) {
-    console.log(outletArgs.model.tags);
-    return outletArgs.model.tags.length > 0;
-  }
+  // static shouldRender(outletArgs, helper) {
+    // console.log(outletArgs.model.tags);
+    // return outletArgs.model.tags.length > 0;
+  // }
   
   get processableTags() {
     return "TAGS: " + settings.valid_tags;
