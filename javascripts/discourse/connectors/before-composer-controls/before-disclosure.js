e506e25ab42952a2a76e731db02c97307f267fd3
Mark Rothlisberger <rothlmar@gmail.com>
2023-11-28 13:56:45 -0500

    Update before-disclosure.js

--- javascripts/discourse/connectors/before-composer-controls/before-disclosure.js
+++ javascripts/discourse/connectors/before-composer-controls/before-disclosure.js
@@ -6,15 +6,15 @@ export default class BeforeDisclosure extends Component {
     // console.log(outletArgs.model.tags);
     // return outletArgs.model.tags.length > 0;
   // }
-  
+
   get tagsAndCategory() {
     // return "TAGS: " + settings.valid_tags;
     return "---" + this.args.outletArgs.model.tags + " " + this.args.outletArgs.model.category.id;
   }
 
   get showDisclosure() {
-    let category_match = this.args.outletArgs.model.category?.id == settings.category_id;
-    
+    let category_match = this.args.outletArgs.model.category?.id === settings.category_id;
+
     let allowed_tags = settings.valid_tags.split("|");
     console.log(allowed_tags, this.args.outletArgs.model.tags);
     let tag_match = false;
