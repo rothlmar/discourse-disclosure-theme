4a9f0c2b0ae291396ce5e63ebc66567050271fc1
Mark Rothlisberger <rothlmar@gmail.com>
2023-11-28 11:30:15 -0500

    Update before-disclosure.js

--- javascripts/discourse/connectors/before-composer-controls/before-disclosure.js
+++ javascripts/discourse/connectors/before-composer-controls/before-disclosure.js
@@ -1,10 +1,7 @@
 import Component from "@glimmer/component";
-import { inject as service } from "@ember/service";
 
 export default class BeforeDisclosure extends Component {
-  @service siteSettings;
-
   get processableTags() {
-    return "TAGS: " + settings.valid_tags    
+    return "TAGS: " + settings.valid_tags;
   }
 }
