5db1cd1da0f197a7c0503ff09b754f94cda2be57
Mark Rothlisberger <rothlmar@gmail.com>
2023-11-28 11:36:07 -0500

    Update before-disclosure.js with shouldRender

--- javascripts/discourse/connectors/before-composer-controls/before-disclosure.js
+++ javascripts/discourse/connectors/before-composer-controls/before-disclosure.js
@@ -4,4 +4,10 @@ export default class BeforeDisclosure extends Component {
   get processableTags() {
     return "TAGS: " + settings.valid_tags;
   }
+
+  static shouldRender(outletArgs, helper) {
+    console.log(helper);
+    console.log(outletArgs.model);
+    return true;
+  }
 }
