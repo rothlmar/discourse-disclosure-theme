9e0fe635c1cacfe5ca42b08ca3728c35ebdc67f5
Mark Rothlisberger <rothlmar@gmail.com>
2023-12-01 16:39:54 -0500

    Update legal-disclosure.js
    fix `map(parseInt)`

--- javascripts/discourse/connectors/composer-after-save-or-cancel/legal-disclosure.js
+++ javascripts/discourse/connectors/composer-after-save-or-cancel/legal-disclosure.js
@@ -2,9 +2,7 @@ import Component from "@glimmer/component";
 
 export default class BeforeDisclosure extends Component {
   get showDisclosure() {
-    console.log("CATEGORY IDS: ", settings.category_ids);
-    let category_ids = settings.category_ids.split("|").map(parseInt);
-    console.log("CATEGORY IDS PARSED", category_ids);
+    let category_ids = settings.category_ids.split("|").map(id => parseInt(id, 10));
     return category_ids.includes(this.args.outletArgs.model.category?.id);
   }
 }
