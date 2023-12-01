5c07207f11f3ed893ef122af23ce0f1ed18f9d2c
Mark Rothlisberger <rothlmar@gmail.com>
2023-12-01 16:35:34 -0500

    Update legal-disclosure.js

--- javascripts/discourse/connectors/composer-after-save-or-cancel/legal-disclosure.js
+++ javascripts/discourse/connectors/composer-after-save-or-cancel/legal-disclosure.js
@@ -4,6 +4,7 @@ export default class BeforeDisclosure extends Component {
   get showDisclosure() {
     console.log("CATEGORY IDS: ", settings.category_ids);
     let category_ids = settings.category_ids.split("|").map(parseInt);
+    console.log("CATEGORY IDS PARSED", category_ids);
     return category_ids.includes(this.args.outletArgs.model.category?.id);
   }
 }
