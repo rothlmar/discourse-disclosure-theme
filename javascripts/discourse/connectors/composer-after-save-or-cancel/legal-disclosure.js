8a4ed3e642dd3cbc21432185677199e8a29851f3
Mark Rothlisberger <rothlmar@gmail.com>
2023-12-01 16:34:12 -0500

    Update legal-disclosure.js

--- javascripts/discourse/connectors/composer-after-save-or-cancel/legal-disclosure.js
+++ javascripts/discourse/connectors/composer-after-save-or-cancel/legal-disclosure.js
@@ -2,6 +2,7 @@ import Component from "@glimmer/component";
 
 export default class BeforeDisclosure extends Component {
   get showDisclosure() {
+    console.log("CATEGORY IDS: ", setting.category_ids);
     let category_ids = settings.category_ids.split("|").map(parseInt);
     return category_ids.includes(this.args.outletArgs.model.category?.id);
   }
